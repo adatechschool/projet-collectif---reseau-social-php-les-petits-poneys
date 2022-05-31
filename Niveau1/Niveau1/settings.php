8f96ab6bf22ae42bbe9bcc9c241d431e1e3d097a
Ladmya <98774256+Ladmya@users.noreply.github.com>
2022-05-31 09:30:56 +0200

    Add files via upload
    Added redirects for header & db connection

--- Niveau1/Niveau1/settings.php
+++ Niveau1/Niveau1/settings.php
@@ -8,22 +8,7 @@
     </head>
     <body>
         <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-
-            </nav>
+        <?php include("/Applications/MAMP/htdocs/Niveau1/Niveau1/header.php"); ?>
         </header>
         <div id="wrapper" class='profile'>
 
@@ -47,11 +32,12 @@
                  * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
                  */
                 $userId = intval($_GET['user_id']);
-
+                ?>
+                <?php
                 /**
                  * Etape 2: se connecter à la base de donnée
                  */
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                include ("/Applications/MAMP/htdocs/Niveau1/Niveau1/db_connection.php");
 
                 /**
                  * Etape 3: récupérer le nom de l'utilisateur
